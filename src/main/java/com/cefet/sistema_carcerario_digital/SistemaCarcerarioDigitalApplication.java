baa66433c9c44c05ffda54b97987d1f68a38d278
deividbss128-pixel <deividbss128@gmail.com>
2026-06-09 19:48:04 -0300

    Initial commit do Spring Initializr

---
+++ src/main/java/com/cefet/sistema_carcerario_digital/SistemaCarcerarioDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.sistema_carcerario_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCarcerarioDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCarcerarioDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cefet/sistema_carcerario_digital/SistemaCarcerarioDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.sistema_carcerario_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCarcerarioDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
